5ec6f6ba5c62d388f6ba5b56a31a561ef03c36a2
Konrad Gawron <konrad.gawron@poczta.fm>
2020-11-03 16:21:40 +0100

    adding more philosophers

--- learning.py
+++ learning.py
@@ -1,4 +1,4 @@
-filozof = ['Arystoteles','Platon','Sokrates', 'Galileusz']
+filozof = ['Arystoteles','Platon','Sokrates', 'Galileusz','Kartezjusz', 'Nietsche']
 liczba_liter = [len(letter) for letter in filozof]
 print(liczba_liter)
 print(len(filozof))
